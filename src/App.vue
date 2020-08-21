3df865c9dccf7217bf5bf0b6963c73927059c765
LeandrodeLimaC <leandrolima7472@gmail.com>
2020-08-21 19:41:46 -0300

    :sparkles: Import of svgIcon and Sprite

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <svg-sprite/>
+    <svg-icon icon="pica-pau"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import SvgSprite from '@/mixins/SvgSprite.vue';
+import SvgIcon from '@/mixins/SvgIcon.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    SvgSprite,
+    SvgIcon
+  },
 }
 </script>
 
